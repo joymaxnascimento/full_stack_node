2786ccf5b94eaf95a90be396dbba5160dabed22e
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2020-07-22 12:13:07 -0300

    Organizacao - Intellij

---
+++ exercicios/encontreUm5.js
@@ -0,0 +1,9 @@
+function encontreUm5(numeros = [1, 2, 3, 4, 5]){
+    do{
+        for (let i = 1; i  <= numeros.length; i++){
+            console.log('Encontramos um 5!')
+            }
+    }while (i === 5)
+}
+
+encontreUm5()
---
+++ exercicios/naoPareDeContarImparesAte.js
@@ -0,0 +1,12 @@
+function naoPareDeContarImparesAte(numero){
+    let numerosImpares = 0
+    for(let i = 0; i <= numero; i++){
+        if(i % 2 != 0){
+            numerosImpares++
+            console.log(i)
+        }
+    }
+    return numerosImpares
+}
+
+console.log(naoPareDeContarImparesAte(7))
