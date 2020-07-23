88aab9201f74a1c125bd9e0b385558a635c91b80
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2020-07-23 17:07:26 -0300

    naoPareDeContarImparesAte.js atualizado

--- exercicios/naoPareDeContarImparesAte.js
+++ exercicios/naoPareDeContarImparesAte.js
@@ -1,4 +1,4 @@
-const naoPareDeContarImparesAte = function(numero){
+function  naoPareDeContarImparesAte(numero){
     let numerosImpares = 0
     for(let i = 0; i <= numero; i++){
         if(i % 2 != 0){
